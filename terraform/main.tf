cb4829b398c18a8f3cb70f457c0bd4a7d6208447
Milan Toth <milan.toth@codium.sk>
2024-02-10 10:25:25 +0100

    Create sandbox-aft Control Tower account

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,10 +2,10 @@ module "sandbox" {
   source = "./modules/aft-account-request"
 
   control_tower_parameters = {
-    AccountEmail              = "<ACCOUNT EMAIL>"
+    AccountEmail              = "milan+afttest@toth-online.com"
     AccountName               = "sandbox-aft"
     ManagedOrganizationalUnit = "Learn AFT"
-    SSOUserEmail              = "<SSO EMAIL>"
+    SSOUserEmail              = "milan+acc@toth-online.com"
     SSOUserFirstName          = "Sandbox"
     SSOUserLastName           = "AFT"
   }
